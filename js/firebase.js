e42d4924bd9e041e5e6dbc4331deb899247d2332
StabiX-Protocol <stabix10@gmail.com>
2026-03-14 21:06:47 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  collection,
+  addDoc,
+  getDocs,
+  serverTimestamp,
+  query,
+  where,
+  orderBy,
+  runTransaction,
+  Timestamp
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+/* ================= FIREBASE ================= */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNs4efcfPoHYk13cU8xuCdnTHOXL1yzT4",
+  authDomain: "stabix-backend-v1.firebaseapp.com",
+  projectId: "stabix-backend-v1",
+  storageBucket: "stabix-backend-v1.firebasestorage.app",
+  messagingSenderId: "351361221507",
+  appId: "1:351361221507:web:ebaf0d15e86d4b184c6cb6"
+};
+
+const appFB = initializeApp(firebaseConfig);
+const db = getFirestore(appFB);
+
 // 🚫 BLOCK NORMAL BROWSER ACCESS (ONLY TELEGRAM ALLOWED)
+
 if (
   !window.Telegram ||
   !window.Telegram.WebApp ||
@@ -24,23 +58,6 @@ if (
       </div>
     </div>
   `;
+
   throw new Error("Not opened inside Telegram");
 }
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import {
-  getFirestore, doc, setDoc, getDoc, updateDoc,
-  collection, addDoc, getDocs, serverTimestamp,
-  query, where, orderBy, runTransaction,
-  Timestamp
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-/* ================= FIREBASE ================= */
-const firebaseConfig = {
-  apiKey: "AIzaSyBNs4efcfPoHYk13cU8xuCdnTHOXL1yzT4",
-  authDomain: "stabix-backend-v1.firebaseapp.com",
-  projectId: "stabix-backend-v1",
-  storageBucket: "stabix-backend-v1.firebasestorage.app",
-  messagingSenderId: "351361221507",
-  appId: "1:351361221507:web:ebaf0d15e86d4b184c6cb6"
-};
-const appFB = initializeApp(firebaseConfig);
-const db = getFirestore(appFB);
