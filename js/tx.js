44c66a9761ef9bc8932af1a2ed3bd053fc4cec90
StabiX-Protocol <stabix10@gmail.com>
2026-04-08 23:26:28 +0530

    Update tx.js

--- js/tx.js
+++ js/tx.js
@@ -216,32 +216,7 @@ if(t.type !== window.filters.type) return;
 if(window.filters.asset){
 if(t.asset !== window.filters.asset) return;
 }
-// ✅ DATE FILTER
 if(window.filters.date){
-  let d = new Date(t.createdAt.seconds * 1000)
-    .toISOString().slice(0,10);
-
-  if(d != window.filters.date) return;
-}
-
-// ✅ RANGE DATE (from-to)
-if(window.filters?.fromDate && window.filters?.toDate){
-  let d = new Date(t.createdAt.seconds * 1000)
-    .toISOString().slice(0,10);
-
-  if(d < window.filters.fromDate || d > window.filters.toDate) return;
-}
-
-if(window.filters.minAmount != null || window.filters.maxAmount != null){
-  const amt = Number(t.amount);
-
-  if(
-    (window.filters.minAmount != null && amt < window.filters.minAmount) ||
-    (window.filters.maxAmount != null && amt > window.filters.maxAmount)
-  ){
-    return;
-  }
-}
 let d = new Date(t.createdAt?.seconds * 1000)
 .toISOString().slice(0,10);
 if(d !== window.filters.date) return;
