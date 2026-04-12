7e57912c3bd189a6e9b47b72295726af6c730a82
StabiX-Protocol <stabix10@gmail.com>
2026-04-13 00:10:37 +0530

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1836,7 +1836,7 @@ window.selectNetwork = function(asset, network){
   <!-- HEADER -->
   <div style="display:flex;align-items:center;gap:10px;margin-bottom:15px;">
     
-    <div onclick="selectNetwork('${asset}', '${name} ${type}')" style="
+    <div onclick="openDeposit('${asset}')" style="
       width:36px;
       height:36px;
       display:flex;
@@ -1977,7 +1977,7 @@ window.selectWithdrawNetwork = function(asset, network){
   <div style="display:flex;align-items:center;gap:10px;margin-bottom:15px;">
     
     <!-- BACK BUTTON -->
-    <div onclick="openWithdrawNetwork('${asset}')" style="
+    <div onclick="SELECTED_NETWORK='${name} ${type}'; openWithdraw('${asset}', '${name} ${type}')" style="
       width:36px;height:36px;
       display:flex;align-items:center;justify-content:center;
       border-radius:10px;
