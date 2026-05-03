e28aa16afc3b51757c831d668b145a91f970c5e5
StabiX-Protocol <stabix10@gmail.com>
2026-05-03 19:57:25 +0530

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -2123,16 +2123,16 @@ window.selectMode = function(mode){
 
   let title = mode === "instant" ? "Notice" : "Self Custody Warning";
 
-  let message = mode === "instant"
-    ? "In Instant mode, deposits are credited to a StabiX managed execution layer where funds are maintained within system controlled hot wallets. User balances are handled internally, enabling fast settlement without repeated on-chain interactions. Withdrawals are executed directly by the backend."
-    : "In Advanced mode, all funds remain on-chain under your control. Deposits and withdrawals happen via smart contracts. Withdrawals use Merkle proofs and batching. You are fully responsible for your funds StabiX does not control them.";
+let message = mode === "instant"
+    ? "In Instant mode, deposits are credited to a StabiX managed execution layer where funds are maintained within system-controlled hot wallets. User balances are handled internally, enabling fast settlement without repeated on-chain interactions. Withdrawals are executed directly by the backend."
+    : "In Advanced mode, all funds remain on-chain under your control. Deposits and withdrawals happen via smart contracts. Withdrawals use Merkle proofs and batching. You are fully responsible for your funds — StabiX does not control them.";
   
-  
-    
   if(confirm(title + "\n\n" + message)){
+    
+    // ✅ ONLY HERE MODE SET HOGA
     window.MODE = mode;
 
-  
+    // UI highlight
     document.getElementById("instantBtn").style.border = "1px solid rgba(255,255,255,0.08)";
     document.getElementById("advancedBtn").style.border = "1px solid rgba(255,255,255,0.08)";
     document.getElementById(mode + "Btn").style.border = "1px solid #3b82f6";
