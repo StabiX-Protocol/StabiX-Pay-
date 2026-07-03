cf927f4a784a3b6130ff7ab251dd458c59f9382d
StabiX-Protocol <stabix10@gmail.com>
2026-07-04 00:27:25 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -5,7 +5,11 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
+  collection,
+  query,
+  where,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const auth = getAuth(window.appFB);
@@ -16,13 +20,23 @@ window.googleLogin = async () => {
     const result = await signInWithPopup(auth, provider);
     const user = result.user;
 
-    localStorage.setItem("stbx_google_uid", user.uid);
+// check existing user by googleUID
+const q = query(
+  collection(db, "users"),
+  where("googleUID", "==", user.uid)
+);
 
-    if (!getCurrentUserId()) {
-      setCurrentUserId(generateSTBX());
-    }
+const snap = await getDocs(q);
 
-    location.reload();
+if (!snap.empty) {
+  const existingUser = snap.docs[0];
+  localStorage.setItem("stbx_uid", existingUser.id);
+  localStorage.setItem("stbx_google_uid", user.uid);
+} else {
+  localStorage.setItem("stbx_google_uid", user.uid);
+}
+
+location.reload();
 
   } catch (e) {
     console.log("Google Login Error:", e);
