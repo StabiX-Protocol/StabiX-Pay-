d44f744de73b9235ff29c36d20dba60e2ce7dd98
StabiX-Protocol <stabix10@gmail.com>
2026-07-21 22:24:20 +0530

    Import transaction-related modules in tx.js
    Added multiple import statements for transaction-related modules.

--- js/Tx/tx.js
+++ js/Tx/tx.js
@@ -1,3 +1,10 @@
+import "./txsend.js";
+import "./loadhistory.js";
+import "./historyrender.js";
+import "./searchhistory.js";
+import "./txdetails.js";
+import "./txpopup.js";
+
 async function generateSTR(){
 
   const systemRef = doc(db, "settings", "system");
