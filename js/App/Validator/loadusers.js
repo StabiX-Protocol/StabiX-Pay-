95966c1adbb4328f1a2eae34ac0481f34589265d
StabiX-Protocol <stabix10@gmail.com>
2026-07-21 22:57:27 +0530

    Update loadusers.js

--- js/App/Validator/loadusers.js
+++ js/App/Validator/loadusers.js
@@ -1,1 +1,35 @@
+window.loadAllUsers = async ()=>{
+const listDiv = document.getElementById("userList");
+const countDiv = document.getElementById("userCount");
+listDiv.innerHTML = "Loading...";
+countDiv.innerHTML = "";
+try{
+const snap = await getDocs(collection(db,"users"));
+countDiv.innerHTML = `Total Users: <b>${snap.size}</b>`;
+let html = "";
+snap.forEach(d=>{
+const u = d.data();
+const username = u.username ? u.username : "No username";
+const stbxId = u.walletAddress ? u.walletAddress : d.id;
+const eoa = u.eoaAddress ? u.eoaAddress : "Not added";
+  
+html += `
+<div class="tx">
+<b>${username}</b><br>
+<span class="small">
+StabiX ID: ${stbxId}
+</span><br>
+<span class="small">
+EOA: ${eoa}
+</span>
+</div>
+`;
+});
 
+listDiv.innerHTML =
+html || "<span class='small'>No users found</span>";
+}catch(e){
+listDiv.innerHTML =
+"<span class='small'>Error loading users</span>";
+}
+};
