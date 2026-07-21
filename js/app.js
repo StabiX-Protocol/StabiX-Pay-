5750eabf0d152020c5ec1662d83e3cca013d32c1
StabiX-Protocol <stabix10@gmail.com>
2026-07-21 10:16:11 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ import "./App/qr.js";
 
 import "./App/navigation/homenav.js";
 import "./App/navigation/deposit.js";
+import "./App/navigation/history.js";
+import "./App/navigation/setting.js";
 
 import "./App/DW/dwnav.js";
 import "./App/DW/network.js";
