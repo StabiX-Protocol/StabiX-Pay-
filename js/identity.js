922c3a26a476b4842027396d06ea522d3e0fd9e8
StabiX-Protocol <stabix10@gmail.com>
2026-07-30 00:10:18 +0530

    Update identity.js

--- js/identity.js
+++ js/identity.js
@@ -14,3 +14,12 @@ window.generateSTBX = function () {
 window.GOOGLE_CLIENT_ID =
   "555121729616-a7a7ertm7i6pgfaps0s2mc9l3v6p6fci.apps.googleusercontent.com";
 
+window.initializeGoogleLogin = function (callback) {
+
+  google.accounts.id.initialize({
+    client_id: window.GOOGLE_CLIENT_ID,
+    callback: callback
+  });
+
+};
+
