b397981fa50b5384a2782a5c2d90ec9913fce940
StabiX-Protocol <stabix10@gmail.com>
2026-08-05 00:22:22 +0530

    jwt auth implement

--- backend/routes/validatorRoutes.js
+++ backend/routes/validatorRoutes.js
@@ -2,6 +2,8 @@ const express = require ("express");
 
 const router = express.Router();
 
+const { verifyToken } = require("../middleware/authMiddleware");
+
 const {
   approveDeposit,
   rejectDeposit,
@@ -12,26 +14,31 @@ const {
 
 router.patch(
   "/deposit/approve/:STRId",
+  verifyToken,
   approveDeposit
 );
 
 router.patch(
   "/deposit/reject/:STRId",
+  verifyToken,
   rejectDeposit
 );
 
 router.get(
   "/deposit/pending",
+  verifyToken,
   getPendingDeposits
 );
 
 router.patch(
   "/withdraw/approve/:STRId",
+  verifyToken,
   approveWithdraw
 );
 
 router.patch(
   "/withdraw/reject/:STRId",
+  verifyToken,
   rejectWithdraw
 );
 
