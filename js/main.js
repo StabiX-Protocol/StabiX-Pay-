e460b389e5f587d9c126cf1fc5e57d8512c62991
StabiX-Protocol <stabix10@gmail.com>
2026-08-18 20:06:24 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ import "./App/navigation/setting.js";
 import "./App/DW/Mode/Instant Mode/instantdeposit.js";
 import "./App/DW/Mode/Instant Mode/instantwithdraw.js";
 import "./App/DW/Mode/Adavance Mode/advancedeposit.js";
-import "./App/DW/Mode/Adavance Mode/advancedwithdraw.js";
+import "./App/DW/Mode/Adavance Mode/advancewithdraw.js";
 
 import "./App/DW/dwnav.js";
 import "./App/DW/network.js";
