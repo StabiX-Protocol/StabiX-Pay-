d4eeddae25732408aaec1743f9604f3ce16911f4
StabiX-Protocol <stabix10@gmail.com>
2026-09-26 22:36:46 +0530

    Legacy Code Removed

--- js/Tx/tx.js
+++
@@ -1,6 +0,0 @@
-import "./txsend.js";
-import "./loadhistory.js";
-import "./historyrender.js";
-import "./searchhistory.js";
-import "./txdetails.js";
-import "./txpopup.js";
--- js/config.js
+++
@@ -1,7 +0,0 @@
-window.STABIX_CONFIG = Object.freeze({
-  API_BASE_URL: "http://10.148.199.19:3000"
-});
-
-window.apiUrl = function(path) {
-  return `${window.STABIX_CONFIG.API_BASE_URL}${path}`;
-};
